74d78843b21a185da3230bdb7eee0f93c247d52f
wfowler1 <wfowler1@mscd.edu>
2011-07-15 07:07:05 +0000

    More work on decompileronly branch. Now that it's mostly working and has a GUI, many changes probably won't be ported back to trunk.
    Added a coefficient option to modify points generated by planar decompilation algorithm. If a map has many broken solids, try changing this value.
    Added an option to use planar decompilation only.
    Added an option to skip checking vertices against the plane. This option is for decompiling custom maps only.
    
    The coefficient option seems to fix most of the remaining problems with decompiling official maps. However there's still more work to do with decompiling custom maps compiled using the compiler tools we currently have.

--- Plane.java
+++ Plane.java
@@ -12,7 +12,7 @@ public class Plane {
 	public final int B=1;
 	public final int C=2;
 	
-	private float[] coefs=new float[3]; // Coefficients in the equation Ax+By+Cz=D
+	private float[] normal=new float[3]; // Coefficients in the equation Ax+By+Cz=D
 	private float dist; // D in the equation
 	private int type; // Still not sure what the fuck this is for. Thought it was an axis, but that'd
 	                  // be pointless since Ax+By+Cz=D is sufficient for any plane ever, as long as A.B.C=1,
@@ -22,18 +22,18 @@ public class Plane {
 	
 	// This one takes the components separate and in the correct data type
 	public Plane(float inA, float inB, float inC, float inDist, int inType) {
-		coefs[A]=inA;
-		coefs[B]=inB;
-		coefs[C]=inC;
+		normal[A]=inA;
+		normal[B]=inB;
+		normal[C]=inC;
 		dist=inDist;
 		type=inType;
 	}
 	
-	public Plane(float[] inCoefs, float inDist, int inType) throws InvalidPlaneException{
-		if(inCoefs.length!=3) {
+	public Plane(float[] inNormal, float inDist, int inType) throws InvalidPlaneException{
+		if(inNormal.length!=3) {
 			throw new InvalidPlaneException();
 		}
-		coefs=inCoefs;
+		normal=inNormal;
 		dist=inDist;
 		type=inType;
 	}
@@ -48,11 +48,11 @@ public class Plane {
 			throw new InvalidPlaneException();
 		}
 		int myInt=(in[3] << 24) | ((in[2] & 0xff) << 16) | ((in[1] & 0xff) << 8) | (in[0] & 0xff);
-		coefs[A]=Float.intBitsToFloat(myInt);
+		normal[A]=Float.intBitsToFloat(myInt);
 		myInt=(in[7] << 24) | ((in[6] & 0xff) << 16) | ((in[5] & 0xff) << 8) | (in[4] & 0xff);
-		coefs[B]=Float.intBitsToFloat(myInt);
+		normal[B]=Float.intBitsToFloat(myInt);
 		myInt=(in[11] << 24) | ((in[10] & 0xff) << 16) | ((in[9] & 0xff) << 8) | (in[8] & 0xff);
-		coefs[C]=Float.intBitsToFloat(myInt);
+		normal[C]=Float.intBitsToFloat(myInt);
 		myInt=(in[15] << 24) | ((in[14] & 0xff) << 16) | ((in[13] & 0xff) << 8) | (in[12] & 0xff);
 		dist=Float.intBitsToFloat(myInt);
 		type=(in[19] << 24) | ((in[18] & 0xff) << 16) | ((in[17] & 0xff) << 8) | (in[16] & 0xff);
@@ -60,14 +60,14 @@ public class Plane {
 	
 	// METHODS
 	public void printPlane() {
-		System.out.println(coefs[A] + " " + coefs[B] + " " + coefs[C] + " " + dist + " " + type);
+		System.out.println(normal[A] + " " + normal[B] + " " + normal[C] + " " + dist + " " + type);
 	}
 	
 	// equals(Plane)
 	// Checks the data of this plane against the data of another, and returns
 	// true if the data is equal in both.
 	public boolean equals(Plane in) {
-		if(coefs[A]==in.getA() && coefs[B]==in.getB() && coefs[C]==in.getC() && dist==in.getDist() && type==in.getType()) {
+		if(normal[A]==in.getA() && normal[B]==in.getB() && normal[C]==in.getC() && dist==in.getDist() && type==in.getType()) {
 			return true;
 		} else {
 			return false;
@@ -77,28 +77,28 @@ public class Plane {
 	// invert()
 	// Negates A, B and C, essentially flipping the plane backward.
 	public void invert() {
-		coefs[A]=coefs[A]*-1;
-		coefs[B]=coefs[B]*-1;
-		coefs[C]=coefs[C]*-1;
+		normal[A]=normal[A]*-1;
+		normal[B]=normal[B]*-1;
+		normal[C]=normal[C]*-1;
 	}
 	
 	// ACCESSORS/MUTATORS
 	
 	// returns the coordinates as a float3
-	public float[] getCoefs() {
-		return coefs;
+	public float[] getNormal() {
+		return normal;
 	}
 	
 	public float getA() {
-		return coefs[A];
+		return normal[A];
 	}
 	
 	public float getB() {
-		return coefs[B];
+		return normal[B];
 	}
 	
 	public float getC() {
-		return coefs[C];
+		return normal[C];
 	}
 	
 	public float getDist() {
@@ -110,15 +110,15 @@ public class Plane {
 	}
 	
 	public void setA(float in) {
-		coefs[A]=in;
+		normal[A]=in;
 	}
 	
 	public void setB(float in) {
-		coefs[B]=in;
+		normal[B]=in;
 	}
 	
 	public void setC(float in) {
-		coefs[C]=in;
+		normal[C]=in;
 	}
 	
 	public void setDist(float in) {
